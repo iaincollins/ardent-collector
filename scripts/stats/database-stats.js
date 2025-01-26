69e33831c7181fccd2a68f7f9d74117841b2f428
Iain Collins <me@iaincollins.com>
2025-01-26 02:34:46 +0000

    Refactor database stats generation script
    This should reduce the imapct of generating stats from the database.
    
    If it performs well enough may re-enable daily stats generation.

--- scripts/stats/database-stats.js
+++ scripts/stats/database-stats.js
@@ -4,8 +4,6 @@ const { getISOTimestamp } = require('../../lib/utils/dates')
 const { systemsDb, locationsDb, stationsDb, tradeDb } = require('../../lib/db')
 
 ;(async () => {
-  const dateTimeOneHourAgo = new Date(new Date().setHours(new Date().getHours() - 1)).toISOString()
-
   console.log('Updating database stats…')
   console.time('Update database stats')
   const commodityStats = tradeDb.prepare(`
@@ -15,37 +13,19 @@ const { systemsDb, locationsDb, stationsDb, tradeDb } = require('../../lib/db')
       (SELECT COUNT(DISTINCT(systemName)) as count FROM commodities) AS tradeSystems,
       (SELECT COUNT(DISTINCT(stationName)) as count FROM commodities WHERE fleetCarrier = 0) AS tradeStations,
       (SELECT COUNT(DISTINCT(stationName)) as count FROM commodities WHERE fleetCarrier = 1) AS tradeCarriers,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @lastHourTimestamp) as updatedInLastHour,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @last24HoursTimestamp) as updatedInLast24Hours,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @last7DaysTimestamp) as updatedInLast7Days,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @last30DaysTimestamp) as updatedInLast30Days,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @last90DaysTimestamp) as updatedInLast90Days,
-      (SELECT COUNT(*) FROM commodities WHERE updatedAt <= @last90DaysTimestamp) as updatedMoreThan90DaysAgo
+      (SELECT COUNT(*) FROM commodities WHERE updatedAt > @last24HoursTimestamp) as updatedInLast24Hours
     FROM commodities
     `).get({
-    lastHourTimestamp: dateTimeOneHourAgo,
-    last24HoursTimestamp: getISOTimestamp(-1),
-    last7DaysTimestamp: getISOTimestamp(-7),
-    last30DaysTimestamp: getISOTimestamp(-30),
-    last90DaysTimestamp: getISOTimestamp(-90)
+    last24HoursTimestamp: getISOTimestamp(-1)
   })
   const stationStats = stationsDb.prepare(`
   SELECT
     (SELECT COUNT(*) FROM stations WHERE stationType != 'Fleet Carrier') as stations,
     (SELECT COUNT(*) FROM stations WHERE stationType = 'Fleet Carrier') as fleetCarriers,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt > @lastHourTimestamp) as updatedInLastHour,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt > @last24HoursTimestamp) as updatedInLast24Hours,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt > @last7DaysTimestamp) as updatedInLast7Days,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt > @last30DaysTimestamp) as updatedInLast30Days,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt > @last90DaysTimestamp) as updatedInLast90Days,
-    (SELECT COUNT(*) FROM stations WHERE updatedAt <= @last90DaysTimestamp) as updatedMoreThan90DaysAgo
+    (SELECT COUNT(*) FROM stations WHERE updatedAt > @last24HoursTimestamp) as updatedInLast24Hours
   FROM stations
   `).get({
-    lastHourTimestamp: dateTimeOneHourAgo,
-    last24HoursTimestamp: getISOTimestamp(-1),
-    last7DaysTimestamp: getISOTimestamp(-7),
-    last30DaysTimestamp: getISOTimestamp(-30),
-    last90DaysTimestamp: getISOTimestamp(-90)
+    last24HoursTimestamp: getISOTimestamp(-1)
   })
   const stats = {
     systems: systemsDb.prepare('SELECT COUNT(*) as count FROM systems').get().count,
@@ -53,32 +33,17 @@ const { systemsDb, locationsDb, stationsDb, tradeDb } = require('../../lib/db')
     stations: {
       stations: stationStats.stations,
       carriers: stationStats.fleetCarriers,
-      updatedInLastHour: stationStats.updatedInLastHour,
-      updatedInLast24Hours: stationStats.updatedInLast24Hours,
-      updatedInLast7Days: stationStats.updatedInLast7Days,
-      updatedInLast30Days: stationStats.updatedInLast30Days,
-      updatedInLast90Days: stationStats.updatedInLast90Days,
-      updatedMoreThan90DaysAgo: stationStats.updatedMoreThan90DaysAgo
+      updatedInLast24Hours: stationStats.updatedInLast24Hours
     },
     trade: {
       systems: commodityStats.tradeSystems,
       stations: commodityStats.tradeStations,
       carriers: commodityStats.tradeCarriers,
       tradeOrders: commodityStats.tradeOrders,
-      updatedInLastHour: commodityStats.updatedInLastHour,
       updatedInLast24Hours: commodityStats.updatedInLast24Hours,
-      updatedInLast7Days: commodityStats.updatedInLast7Days,
-      updatedInLast30Days: commodityStats.updatedInLast30Days,
-      updatedInLast90Days: commodityStats.updatedInLast90Days,
-      updatedMoreThan90DaysAgo: commodityStats.updatedMoreThan90DaysAgo,
       uniqueCommodities: commodityStats.uniqueCommodities
     },
-    updatedInLastHour: commodityStats.updatedInLastHour + stationStats.updatedInLastHour,
     updatedInLast24Hours: commodityStats.updatedInLast24Hours + stationStats.updatedInLast24Hours,
-    updatedInLast7Days: commodityStats.updatedInLast7Days + stationStats.updatedInLast7Days,
-    updatedInLast30Days: commodityStats.updatedInLast30Days + stationStats.updatedInLast30Days,
-    updatedInLast90Days: commodityStats.updatedInLast90Days + stationStats.updatedInLast90Days,
-    updatedMoreThan90DaysAgo: commodityStats.updatedMoreThan90DaysAgo + stationStats.updatedMoreThan90DaysAgo,
     timestamp: new Date().toISOString()
   }
   if (!fs.existsSync(ARDENT_CACHE_DIR)) { fs.mkdirSync(ARDENT_CACHE_DIR, { recursive: true }) }
